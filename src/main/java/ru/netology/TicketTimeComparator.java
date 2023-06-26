d250a0a65f9a38eb32371c7c8f90a9980d12c385
Maryia <tsapkova_mariya@bk.ru>
2023-06-26 21:46:20 +0500

    #1

---
+++ src/main/java/ru/netology/TicketTimeComparator.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        int time1 = t1.getTimeTo() - t2.getTimeFrom();
+        int time2 = t2.getTimeTo() - t1.getTimeFrom();
+        return time2 - time1;
+    }
+}
